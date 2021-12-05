a650f912d1f907592613ded89d4609a22eb866ea
Vitalii Morgunov <utlab13@gmail.com>
2021-12-05 15:02:44 +0100

    Add page pagination

--- controllers/contacts/getContacts.js
+++ controllers/contacts/getContacts.js
@@ -2,10 +2,12 @@ const { Contact } = require('../../models');
 
 const getContacts = async (req, res) => {
   const { _id } = req.user;
-  const contacts = await Contact.find({ owner: _id }).populate(
-    'owner',
-    '_id email subscription'
-  );
+  const { page = 1, limit = 10 } = req.query;
+  const skip = (page - 1) * limit;
+  const contacts = await Contact.find({ owner: _id }, '', {
+    skip,
+    limit: Number(limit),
+  }).populate('owner', '_id email subscription');
   res.status(200).json({
     status: 'success',
     message: 'Show all contacts',
